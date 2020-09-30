60e337bc2203ff52da106e6e9244a70a6991a730
Anirban <anirbandas52134@fmail.com>
2020-09-30 22:17:14 +0530

    Dashboard and Create Note Activity UI Added

--- app/src/main/java/com/example/notesapplication/MainActivity.java
+++ app/src/main/java/com/example/notesapplication/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.notesapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    //This error code will notify if there is any problem while passing data between intent
+    public static final int REQUEST_CODE_ADD_NOTE = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView addNoteMain = findViewById(R.id.addNoteMain);
+        addNoteMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivityForResult(
+                        new Intent(getApplicationContext(), CreateNoteActivity.class),
+                        REQUEST_CODE_ADD_NOTE
+                );
+            }
+        });
     }
 }
